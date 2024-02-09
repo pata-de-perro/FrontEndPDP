2ab882bbe597dd00a1ab06b6febb279001ecdeef
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-02-09 17:10:02 -0600

    efects on nav bar items

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,105 +1,162 @@
 import clsx from "clsx";
-import Link from "next/link";
-// import logo from "../image/dePataDePerroLogo.svg"
+import { MenuItems } from "./menuItems"; 
+import {Link} from "next/link";
+import logo from "../image/dePataDePerroLogo.svg"
 
 export default function Navbar(){
     return(
-        <nav className={clsx(
-            "bg-accent1",
-            "fixed",
-            "w-full",
-            "z-20",
-            "top-0 start-0",
-            "border-b"
-        )}>
-            <div className={clsx(
-                "max-w-screen-xl",
-                "flex flex-wrap",
-                "items-center justify-between",
-                "mx-auto p-4"
+        <header>
+            <nav className={clsx(
+            
+            )}>
+                <div className={clsx(
+                    "flex flex-row",
+                    "mx-[30px] my-[25px]",
+                    "px-[11px] py-[11px]",
+                    "bg-accent1",
+                    "rounded-full",
                 )}>
-                    <a href="#" className={clsx(
-                        "flex",
-                        "items-center",
-                        "space-x-3",
-                        "rtl:space-x-reverse"
-                    )}>
-                        <img src="../image/DPDP-Logo1.svg" className={clsx(
-                            "h-8"
-                        )} alt="DPDP-Logo1.svg"></img>
-                        <span className={clsx(
-                            "self-center",
-                            "text-h1 text-primary font-logoFont",
-                            // "stroke-[3px] stroke-white",
-                            "whitespace-nowrap"
-                        )}>de pata de perro</span>
-                    </a>
-            </div>
+                    <img src="../image/dePataDePerroLogo.svg"></img>
+                    <p className={clsx(
+                        "font-logoFont",
+                        "text-h1",
+                        "text-primary"
+                    )}>depatadeperro</p>
 
-        </nav>
-    
+                    <div>
+                        <ul className="menu-list">
+                            {MenuItems.map((item, index) => {
+                                return (
+                                    <li key={index}>
+                                        <a className={item.cName} href={item.url}>
+                                            {item.title}
+                                        </a>
+                                    </li>
+                                )
+                            })}
+                            <button className={clsx(
+                                "bg-none",
+                                "text-accent2 text-regular",
+                                "px-5 py-2",
+                                "rounded-full",
+                                "hover:text-primary hover:text-regularBold",
+                            )}>¡Registrate!</button>
+                            <button className={clsx(
+                            "bg-accent2",
+                            "text-white",
+                            "px-5 py-2",
+                            "rounded-full",
+                            "hover:bg-primary"
+                        )}>Iniciar sesión</button>
+                        </ul>
+                    </div>
+                </div>
+
+            </nav>  
+        </header>
+        
         );
 }
 
-        // <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-        //     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        //         <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-        //             <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo">
-        //             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-        //         </a>
-        //         <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-        //             <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
-        //             <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-        //                 <span class="sr-only">Open main menu</span>
-        //                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-        //                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        //                 </svg>
-        //             </button>
-        //         </div>
-        //         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-        //             <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        //             <li>
-        //                 <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
-        //             </li>
-        //             <li>
-        //                 <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-        //             </li>
-        //             <li>
-        //                 <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-        //             </li>
-        //             <li>
-        //                 <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-        //             </li>
-        //             </ul>
-        //         </div>
-        //     </div>
-        // </nav>
-
-
-    {/* // <nav className={clsx(
-        //     'bg-accent1'
-        //     )}>
+        // <nav className={clsx(
+        //     "bg-accent1",
+        //     // "fixed", //fixed es el que hace que el texto se pase para atrás
+        //     // "w-full",
+        //     "z-20",
+        //     // "top-0 start-0",
+        //     // "border-b"
+        // )}>
         //     <div className={clsx(
-        //         // "max-w-lg",
-        //         "rounded-full",
-        //         // "w-25"
-        //     )}>
-        //         <div className={clsx(
-        //             "flex h-14",
-        //             "m-4", 
-        //             "items-center", 
-        //             "justify-between",
-                    
+        //         "w-[92%]",
+        //         "flex flex-wrap",
+        //         "items-center justify-between",
+        //         "mx-auto p-4",
         //         )}>
-        //             <img className={clsx(
-        //                 "h-auto",
-        //                 "max-w-full"
-        //             )} src="../image/dePataDePerroLogo.svg" alt="dePataDePerroLogo.svg"></img>
-        //             <p className={clsx(
-        //                 "text-h1 font-body",
-        //             )}>Hola mundo</p>
-
-        //         </div>
-
+        //             <a href="#" className={clsx(
+        //                 "flex",
+        //                 "items-center",
+        //                 "space-x-3",
+        //                 "rtl:space-x-reverse"
+        //                 )}>
+        //                 <img src="../image/DPDP-Logo1.svg" className={clsx(
+        //                     "h-8",
+        //                     "w-16",
+        //                     )} alt="DPDP-Logo1.svg"></img>
+        //                 <span className={clsx(
+        //                     "self-center",
+        //                     "text-h1 text-primary font-logoFont",
+        //                     // "stroke-[3px] stroke-white",
+        //                     "whitespace-nowrap"
+        //                     )}>de pata de perro</span>
+        //             </a>
+        //             <div className={clsx(
+        //                 "absoulte", 
+        //                 "bg-black",
+        //                 "min-h-[60vh]",
+        //                 "md:min-h-fit",
+        //                 "left-0",
+        //                 "top-[9%]",
+        //                 "w-full",
+        //                 "md:items-center",
+        //                 "gap-2",
+        //                 "md:gap-[4vw]",
+        //                 "px-5 ",
+        //             )}>
+        //                 <ul className={clsx(
+        //                     "flex",
+        //                     // "md:flex-row",
+        //                     "items-center",
+        //                     "gap-[4vw]",
+        //                     "text-accent2",
+        //                 )}>
+        //                     <li>
+        //                         <a href="#" className={clsx(
+        //                             "hover:text-primary",
+        //                         )}>
+        //                             Quienes somos
+        //                         </a>
+        //                     </li>
+        //                     <li>
+        //                         <a href="#" className={clsx(
+        //                             "hover:text-primary",
+        //                         )}>
+        //                             Nuestras funciones
+        //                         </a>
+        //                     </li>
+        //                     <li>
+        //                         <a href="#" className={clsx(
+        //                             "hover:text-primary",
+        //                         )}>
+        //                             Opiniones
+        //                         </a>
+        //                     </li>
+        //                     <li>
+        //                         <a href="#" className={clsx(
+        //                             "hover:text-primary",
+        //                         )}>
+        //                             Tutoriales
+        //                         </a>
+        //                     </li>
+        //                     <li>
+        //                         <a href="#" className={clsx(
+        //                             "hover:text-primary",
+        //                         )}>
+        //                             Registrate
+        //                         </a>
+        //                     </li>
+        //                 </ul>
+        //             </div>
+        //             <div>
+        //                 <button className={clsx(
+        //                     "bg-accent2",
+        //                     "text-white",
+        //                     "px-5 py-2",
+        //                     "rounded-full",
+        //                     "hover:bg-primary"
+        //                 )}>
+        //                     Iniciar sesión
+        //                 </button>
+        //             </div>
         //     </div>
-        // </nav> */}
+
+        // </nav>
---
+++ src/app/components/menuItems.jsx
@@ -0,0 +1,28 @@
+export const MenuItems=[
+    {
+        title: "Quienes somos",
+        url:"#",
+        cName:"nav-links text-regular hover:text-regularBold text-accent2 hover:text-primary",
+    },
+    {
+        title: "Nuestras funciones",
+        url:"#",
+        cName:"nav-links text-regular hover:text-regularBold text-accent2 hover:text-primary",
+    },
+    {
+        title: "Opiniones",
+        url:"#",
+        cName:"nav-links text-regular hover:text-regularBold text-accent2 hover:text-primary",
+    },
+    {
+        title: "Tutoriales",
+        url:"#",
+        cName:"nav-links text-regular hover:text-regularBold text-accent2 hover:text-primary",
+    },
+]
+
+// {
+//     title: "",
+//     url:"#",
+//     cName:"nav-links",
+// },
