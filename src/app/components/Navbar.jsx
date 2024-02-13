6371d665ce74f50b43e4873ff4a2f0d5fb62101c
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-02-12 18:05:36 -0600

    Correct positions of items in navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -16,11 +16,11 @@ export default function Navbar() {
             "grid",
             "grid-cols-2",
             "items-center",
+            "md:grid-cols-3",
             )}>
             <div className={clsx(
                 "logoContainer",
-                // "flex",
-                // "flex-wrap",
+
                 )}>
                 <Image 
                 src="/DePataDePerroIcon.svg"
@@ -47,13 +47,75 @@ export default function Navbar() {
                     "md:h-[60px]",
                     )}
                     />
-
             </div>
 
+            <div className={clsx(
+                "justify-end",
+                "md:col-span-2",
+            )}>
+                <ul className={clsx(
+                    "flex",
+                    "flex-wrap",
+                    "lg:justify-around",
+                    "md:justify-end",
+                    "items-center",
+                    "w-full",
+                )}>
+                    <li className="sr-only lg:not-sr-only">
+                        <a className={clsx(
+                            "font-body",
+                            "text-accent2",
+                            "text-regular",
+                            "hover:text-primary",
+                        )}>Funciones</a>
+                    </li>
+                    <li className="sr-only lg:not-sr-only">
+                        <a className={clsx(
+                            "font-body",
+                            "text-accent2",
+                            "text-regular",
+                            "hover:text-primary",
+                        )}>Opiniones</a>
+                    </li>
+                    <li className="sr-only lg:not-sr-only">
+                        <a className={clsx(
+                            "font-body",
+                            "text-accent2",
+                            "text-regular",
+                            "hover:text-primary",
+                        )}>Tutoriales</a>
+                    </li>
+                    <li className={clsx(
+                        "sr-only",
+                        "md:not-sr-only",
+                        // "md:justify-items-end",
+                    )}>
+                        <button className={clsx(
+                        "bg-none",
+                        "text-accent2 text-regular",
+                        "px-[8px] py-[8px]",
+                        "w-[138px]",
+                        "mx-2",
+                        // "md:justify-self-end",
+                        "rounded-full",
+                        "hover:text-primary",
+                        "border-2 border-accent2 hover:border-primary",
+                    )}>¡Registrate!</button>
+                        <button className={clsx(
+                            "bg-accent2",
+                            "text-white text-regular",
+                            "px-[8px] py-[8px]",
+                            "w-[138px]",
+                            "rounded-full",
+                            "hover:bg-primary",
+                            "border-2 border-accent2 hover:border-primary",
+                        )}>Iniciar sesión</button>
+                    </li>
             <div className={clsx(
                 "mobileMenu",
                 "justify-self-end",
                 "mr-[24px]",
+                "lg:sr-only",
             )}>
                 <Image 
                 src="/mobile-menu.svg"
@@ -61,10 +123,14 @@ export default function Navbar() {
                 height={24}
                 alt="Mobile menu icon"
                 className={clsx(
-
+                    "cursor-pointer",
                 )}
                 />
             </div>
+                </ul>
+            </div>
+
+            
         </div>
 
        </nav>
