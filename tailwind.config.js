36466bc38d5e57dc3b86d3fc09178e84889f336b
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-02-13 18:30:41 -0600

    commented lines deleted

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,8 +45,6 @@ module.exports = {
         'heading': ['Montserrat'],
         'body': ['Source Sans 3'],
         'logoFont': ['Protest Riot'],
-      //   sans: [ ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-      //   serif: [ui-serif, Georgia, Cambria, "Times New Roman", Times, serif],
       },
       fontSize:{
         'h1':['1.875rem', {
