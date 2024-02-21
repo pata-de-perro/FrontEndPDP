6a4d7e46c11519f4792383860336addcf75301b1
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-02-20 20:23:56 -0600

    Aligning items in md screen

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,66 +6,85 @@ export default function Footer() {
   return (
     <footer
       className={clsx(
-        "bg-accent2", 
-        "mt-[24px]", 
-        "flex", 
+        "bg-accent2",
+        "mt-[24px]",
+        "flex",
         "justify-center",
-        "w-[100vw]"
-        )}
+        "w-[100vw]",
+        "md:grid-cols-2"
+      )}
     >
-      <div className={clsx(
-        "pt-[24px]", 
-        "w-[500px]",
-        "flex flex-wrap flex-col",
-            // "justify-center",
+      <div
+        className={clsx(
+          "m-[5px]",
+          "pt-[24px]",
+          "w-[500px]",
+          "flex flex-wrap flex-col",
+          "md:w-[100vw]",
+          "md:flex-no-wrap"
+        )}
+      >
+        <div className={clsx(
+            "m-[5px]",
+            "md:col-span-1",
+            "md:flex flex-row-reverse",
+            "md:justify-between",
         )}>
-        <Image
-          src="/dePataDePerroLogo.svg"
-          width={217}
-          height={41}
-          alt="Footer logo"
-          className={clsx(
+          <div>
+          <Image
+            src="/dePataDePerroLogo.svg"
+            width={217}
+            height={41}
+            alt="Footer logo"
+            className={clsx(
               "grid justify-self-center",
-              // "justify-self-center",
-          )
-          }
-        />
-        <div
-          className={clsx(
-            "grid",
-            "grid-cols-4",
-            "mt-[15px]",
-            "justify-items-center"
-          )}
-        >
-          <Image
-            src="./instagram.svg"
-            width={24}
-            height={24}
-            alt="Instagram icon"
+              "md:flex",
+              "md:justify-end"
+            )}
           />
-          <Image src="./tiktok.svg" width={24} height={24} alt="Tiktok icon" />
-          <Image
-            src="./youtube.svg"
-            width={24}
-            height={24}
-            alt="Youtube icon"
-          />
-          <Image
-            src="./twitter-x.svg"
-            width={24}
-            height={24}
-            alt="Twitter X icon"
-          />
-        </div>
-
-        <div>
-          <ul
+          <div
+            className={clsx(
+              "grid",
+              "grid-cols-4",
+              "mt-[15px]",
+              "justify-items-center"
+            )}
+          >
+            <Image
+              src="./instagram.svg"
+              width={24}
+              height={24}
+              alt="Instagram icon"
+            />
+            <Image
+              src="./tiktok.svg"
+              width={24}
+              height={24}
+              alt="Tiktok icon"
+            />
+            <Image
+              src="./youtube.svg"
+              width={24}
+              height={24}
+              alt="Youtube icon"
+            />
+            <Image
+              src="./twitter-x.svg"
+              width={24}
+              height={24}
+              alt="Twitter X icon"
+            />
+          </div>
+          </div>
+        <ul
             className={clsx(
               "text-white text-regularBold font-body",
               "text-center",
               "mt-[15px]",
-              "grid grid-cols-2"
+              "grid grid-cols-2",
+              "m-[5px]",
+              "md:grid-cols-1",
+              "md:text-start",
             )}
           >
             <li className="p-[5px]">
@@ -82,13 +101,21 @@ export default function Footer() {
             </li>
           </ul>
         </div>
-
-        <div>
+        
+        <div className={clsx(
+            "border border-sky-200",
+            "m-[5px]",
+            "md:flex flex-row-reverse",
+            "md:items-center",
+            "md:m-[15px]",
+            "md:justify-between",
+        )}>
           <ul
             className={clsx(
               "text-regular text-white font-body text-center",
               "grid grid-cols-3",
               "m-[15px]",
+              "md:justify-around",
             )}
           >
             <li>
@@ -104,7 +131,9 @@ export default function Footer() {
           <p
             className={clsx(
               "text-regular font-body text-white text-center",
-              "mb-[25px]"
+              "mb-[25px]",
+              "md:text-start",
+              "md:self-center",
             )}
           >
             © 2024 | PataDePerro
