89211423d554d9ed300477b810f3032ca4f7b203
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-02-26 12:30:25 -0600

    routing of login and register page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,10 @@
+import clsx from "clsx";
+import Image from "next/image";
+
+export default function Login() {
+    return (
+        <main>
+            <p>LOGIN</p>
+        </main>
+    )
+}
---
+++ src/app/register/page.jsx
@@ -0,0 +1,8 @@
+import clsx from "clsx";
+import Image from "next/image";
+
+export default function Register() {
+    return (
+        <p>Aquí va el registro</p>
+    )
+}
