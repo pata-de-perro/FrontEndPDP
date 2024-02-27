acb63d7f6a93ddeddd5dbeedbc3f45b116ff3967
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-02-26 21:08:01 -0600

    placing images in correct position in lg screen

--- src/app/components/Tutorials.jsx
+++ src/app/components/Tutorials.jsx
@@ -83,7 +83,7 @@ export default function Tutorials() {
           alt="Hand with phone screen sm"
         ></Image>
         <Image
-          className={clsx("z-40", "sr-only", "md:not-sr-only", "lg:sr-only",)}
+          className={clsx("z-40", "sr-only", "md:not-sr-only", "lg:sr-only")}
           src="/Hand-md.svg"
           width={500}
           height={100}
@@ -121,68 +121,78 @@ export default function Tutorials() {
         className={clsx(
           "flex",
           "flex-row",
+          // "grid grid-cols-3",
           "sr-only",
           "lg:not-sr-only",
           "full-size",
-          "relative"
+          "relative",
+          "bg-cover bg-no-repeat"
         )}
       >
-        <Image
-          className={clsx("mt-7", "md:mt-4", "z-10")}
-          src="/Hand-lg.svg"
-          width={1200}
-          height={100}
-          alt="hand with phone screen lg"
-        ></Image>
-        <div
-          className={clsx(
-            "flex",
-            "flex-row",
-            "full-size",
-            "relative top-20 left-0 right-0",
-            "z-50"
-          )}
-        >
-          <div
-            className={clsx(
-              "flex",
-              "items-center",
-              "justify-center",
-              "flex-col",
-              "z-0"
-            )}
-          >
-            <p className={clsx("text-h3", "text-accent2", "drop-shadow-xl")}>
-              Cómo funciona
-            </p>
+        <div className="relative w-[100vw] h-[100px] mb-[800px]">
+          <div className="absolute -z-10 h-full left-0">
             <Image
-              className={clsx("mt-7", "md:mt-4")}
-              src="/Video-cover.svg"
-              width={350}
+              className={clsx("mt-7", "-z-10", "w-full")}
+              src="/Hand-lg.svg"
+              width={1200}
               height={100}
-              alt="Vertical road pic"
+              alt="hand with phone screen lg"
             ></Image>
           </div>
+
           <div
             className={clsx(
-              "flex",
-              "items-center",
-              "justify-center",
-              "flex-col",
-              "flex",
-              "flex-col-reverse"
+              // "flex",
+              // "flex-row",
+              "full-size",
+              "relative top-20 left-0 right-0",
+              "z-50"
             )}
           >
-            <p className={clsx("text-accent2", "text-h3", "deop-shadow-2xl")}>
-              Agregando experiencias
-            </p>
-            <Image
-              className={clsx("mt-4", "z-10")}
-              src="/Video-cover2.svg"
-              width={350}
-              height={100}
-              alt="Vertical road pic"
-            ></Image>
+            <div
+              className={clsx(
+                "flex",
+                "items-center",
+                "justify-center",
+                "flex-col",
+                "pr-10",
+                "border-2 border-black"
+              )}
+            >
+              <p className={clsx("text-h3", "text-accent2", "drop-shadow-xl")}>
+                Cómo funciona
+              </p>
+              <Image
+                className={clsx("mt-7 mb-0", "md:mt-4")}
+                src="/Video-cover.svg"
+                width={350}
+                height={100}
+                alt="¿Cómo funciona? video cover"
+              ></Image>
+            </div>
+            <div
+              className={clsx(
+                "flex",
+                "items-end",
+                "justify-center",
+                "flex-col",
+                "flex",
+                "flex-col-reverse",
+                "border-2 border-black",
+                "ml-2"
+              )}
+            >
+              <p className={clsx("text-accent2", "text-h3", "deop-shadow-2xl")}>
+                Agregando experiencias
+              </p>
+              <Image
+                className={clsx("mt-4", "z-10")}
+                src="/Video-cover2.svg"
+                width={350}
+                height={100}
+                alt="Agregando experiencias video cover"
+              ></Image>
+            </div>
           </div>
         </div>
       </div>
