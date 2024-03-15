266e48ecc75a687996c7ecc611340c7abf07f949
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-03-15 00:18:07 -0600

    saving changes on profile

--- src/app/mx/profile/page.jsx
+++ src/app/mx/profile/page.jsx
@@ -4,124 +4,70 @@ import { HeroAdmin } from "@/app/components/common";
 import { NotInvite } from "@/app/components/events/NotInvite";
 
 export default function Profile() {
-    const dataUser =[
-        {
-            title:"Amigos",
-            num:5,
-        },
-        {
-            title:"Salidas",
-            num:8,
-        },
-        {
-            title:"Eventos",
-            num:1,
-        },
-    ]
   return (
     <>
-    <main>
-        <TitleSection 
-        urlIcon="/person_icon.svg"
-        title="Perfil"
-        />
-        <HeroAdmin 
-        title="Nina Vidales"
-        >
-        <p className="font-heading text-regularSemiBold">ninavidales@email.com</p>
-        <p className="font-heading text-regularSemiBold">331245678</p>
+      <main>
+        <TitleSection urlIcon="/person_icon.svg" title="Perfil" />
+        <HeroAdmin title="Nina Vidales">
+          <p className="font-heading text-regularSemiBold">
+            ninavidales@email.com
+          </p>
+          <p className="font-heading text-regularSemiBold">331245678</p>
         </HeroAdmin>
         <section className="mt-4">
-            <div className={clsx(
-                // "mb-16",
-                "flex justify-start"
-            )}>
-                <button      
-                    className={clsx(
-                    "w-[180px] h-[45px]",
-                    "rounded-xl",
-                    "mx-2",
-                    "bg-azulGris500",
-                    "font-body text-regular text-white",
-                    // "justify-start",
-                    "absolute right-4 -bottom-10",
-                    "md:static"
-                    )}>Editar</button>
-                <button
-                      className={clsx(
-                        "w-[180px] h-[45px]",
-                        "rounded-xl",
-                        "mx-2",
-                        "bg-secondary",
-                        "font-body text-regular text-accent2",
-                        "justify-center",
-                        "absolute right-4 -bottom-10",
-                        "md:static"
-                      )}>Cambiar imagen</button>
+          <div className={clsx("flex justify-start")}>
+            <button
+              className={clsx(
+                "mx-2",
+                "w-[180px] h-[45px]",
+                "font-body text-regular text-white",
+                "absolute right-4 -bottom-10",
+                "rounded-xl",
+                "bg-azulGris500",
+                "md:static"
+              )}
+            >
+              Editar
+            </button>
 
-                      <div className="static">
-                        <img src="/profile-pic.png" width={150} height={150}
-                        className={clsx(
-                            "absolute z-10 right-10 top-16",
-                        )}/>
-                      </div>
+            <button
+              className={clsx(
+                "w-[180px] h-[45px]",
+                "rounded-xl",
+                "mx-2",
+                "bg-secondary",
+                "font-body text-regular text-accent2",
+                "justify-center",
+                "absolute right-4 -bottom-10",
+                "md:static"
+              )}
+            >
+              Cambiar imagen
+            </button>
+
+            <div className="static">
+              <img
+                src="/profile-pic.png"
+                width={150}
+                height={150}
+                className={clsx("absolute z-10 right-10 top-16")}
+              />
             </div>
-            {/* <div className="grid grid-cols-1 justify-items-center w-fit bg-accent1">
-                <div className={clsx(
-                    "w-[200px] h-[200px]",
-                    "rounded-full",
-                    // "flex justify-center",
-                    "bg-red-500"
-                )}></div>
-                <span className={clsx(
-                    "grid grid-cols-3 justify-items-end",
-                    // "ml-2",
-                    "w-fit",
-                    // "bg-red-500"
-                )}>
-                    {dataUser.map((item, index) => {
-                        return(
-                            <div key={index}>
-                                <h2 className={clsx(
-                                    "font-heading text-h4",
-                                    "text-accent2 text-center",
-                                    // "border-2"
-                                    )}>{item.num}</h2>
-                                <p className={clsx(
-                                    "font-body text-regular",
-                                    "mx-4",
-                                    // "border",
-                                )}>{item.title}</p>
-                                 </div>
-                        )
-                    })}
-                </span>
-            </div> */}
+          </div>
         </section>
-        {/* <BtnPrimary 
-        title="Editar"
-        onClick={onclick}
-        /> */}
 
-        <TitleSection 
-        urlIcon="/friends-icon.svg"
-        title="Amigos"
+        <TitleSection urlIcon="/friends-icon.svg" title="Amigos" />
+        <section className="h-[150px]"></section>
+
+        <TitleSection
+          urlIcon="/Icono-Fecha.svg"
+          title="Invitaciones pendientes de aceptar"
         />
-        <section className="h-[150px]">
-
-        </section>
-
-        <TitleSection 
-        urlIcon="/Icono-Fecha.svg"
-        title="Invitaciones pendientes de aceptar"
-        />      
 
         <section className="w-full">
-            <NotInvite />
+          <NotInvite />
         </section>
-
-    </main>
+      </main>
     </>
-
   );
 }
