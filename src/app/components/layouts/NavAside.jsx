5d79ab805680d649f8aa13c609bc02b3555625c3
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-03-15 14:38:06 -0600

    saving links

--- src/app/components/layouts/NavAside.jsx
+++ src/app/components/layouts/NavAside.jsx
@@ -1,128 +1,139 @@
 import Image from "next/image";
 import clsx from "clsx";
 
+import Link from "next/link";
+
 export function NavAside() {
   return (
-    <nav
-      className=" p-6 w-full flex flex-col flex-wrap"
-      data-hs-accordion-always-open
-    >
-      <ul className="space-y-1.5">
-        <li className={clsx("mb-2")}>
-          <a
+    <nav className="p-6 w-full flex flex-col flex-wrap h-full"
+    data-hs-accordion-always-open
+  >
+    <ul className="space-y-1.5">
+
+      <li className={clsx("mb-2")}>
+        <Link
+          className={clsx(
+            "flex items-center",
+            "gap-x-3.5",
+            "py-2 px-2.5",
+            "bg-gray-100",
+            "text-sm",
+            "rounded-lg",
+            "hover:bg-accent1"
+          )}
+          href="/mx/home"
+        >
+          <Image
+            src="/House-icon.svg"
+            width={24}
+            height={24}
+            alt="Mobile menu icon"
+            className={clsx("ml-[15px]", "hover:stroke-primary")}
+          />
+          Inicio
+        </Link>
+      </li>
+      <li className={clsx("mb-2")}>
+        <Link
+          className={clsx(
+            "flex items-center",
+            "gap-x-3.5",
+            "py-2 px-2.5",
+            "bg-gray-100",
+            "text-sm",
+            "rounded-lg",
+            "hover:bg-accent1"
+          )}
+          href="/mx/profile"
+        >
+          <Image
+            src="/person_icon.svg"
+            width={24}
+            height={24}
+            alt="Person aside icon"
+            className={clsx("ml-[15px]", "hover:stroke-primary")}
+          />
+          Tu perfil
+        </Link>
+      </li>
+      <li className={clsx("mb-2")}>
+        <a
+          className={clsx(
+            "flex",
+            "items-center",
+            "gap-x-3.5",
+            "py-2 px-2.5",
+            "bg-gray-100",
+            "text-sm",
+            "rounded-lg",
+            "hover:bg-accent1"
+          )}
+        >
+          <Image
+            src="/Calender-icon.svg"
+            width={24}
+            height={24}
+            alt="Calender aside icon"
+            className={clsx("ml-[15px]", "hover:stroke-primary")}
+          />
+          Tus eventos
+        </a>
+      </li>
+      <li className={clsx("mb-2")}>
+        <a
+          className={clsx(
+            "flex",
+            "items-center",
+            "gap-x-3.5",
+            "py-2",
+            "px-2.5",
+            "bg-gray-100",
+            "text-sm",
+            "rounded-lg",
+            "hover:bg-accent1"
+          )}
+        >
+          <Image
+            src="/Tutorials-icon.svg"
+            width={24}
+            height={24}
+            alt="Tutorials aside icon"
+            className={clsx("ml-[15px]", "hover:stroke-primary")}
+          />
+          Tutorials
+        </a>
+      </li>
+      <li className={clsx("mb-2", )}>
+        <a
+          className={clsx(
+            "flex",
+            "items-center",
+            "gap-x-3.5",
+            "py-2",
+            "px-2.5",
+            "bg-gray-100",
+            "text-sm",
+            "rounded-lg",
+            "hover:bg-accent1"
+          )}
+        >
+          <Image
+            src="/Logout-icon.svg"
+            width={24}
+            height={24}
+            alt="Logout aside icon"
             className={clsx(
-              "flex items-center",
-              "gap-x-3.5",
-              "py-2 px-2.5",
-              "bg-gray-100",
-              "text-sm",
-              "rounded-lg",
-              "hover:bg-accent1"
-            )}
-          >
-            <Image
-              src="/House-icon.svg"
-              width={24}
-              height={24}
-              alt="Mobile menu icon"
-              className={clsx("ml-[15px]", "hover:stroke-primary")}
-            />
-            Inicio
+              "ml-[15px]", 
+              "flex items-baseline",
+              "hover:stroke-primary"
+              )}
+          />
+          Cerrar sesión
           </a>
-        </li>
-        <li className={clsx("mb-2")}>
-          <a
-            className={clsx(
-              "flex items-center",
-              "gap-x-3.5",
-              "py-2 px-2.5",
-              "bg-gray-100",
-              "text-sm",
-              "rounded-lg",
-              "hover:bg-accent1"
-            )}
-          >
-            <Image
-              src="/Car-icon.svg"
-              width={24}
-              height={24}
-              alt="Car aside icon"
-              className={clsx("ml-[15px]", "hover:stroke-primary")}
-            />
-            Tus viajes
-          </a>
-        </li>
-        <li className={clsx("mb-2")}>
-          <a
-            className={clsx(
-              "flex",
-              "items-center",
-              "gap-x-3.5",
-              "py-2 px-2.5",
-              "bg-gray-100",
-              "text-sm",
-              "rounded-lg",
-              "hover:bg-accent1"
-            )}
-          >
-            <Image
-              src="/Fork-icon.svg"
-              width={24}
-              height={24}
-              alt="Fork aside icon"
-              className={clsx("ml-[15px]", "hover:stroke-primary")}
-            />
-            Tus salidas
-          </a>
-        </li>
-        <li className={clsx("mb-2")}>
-          <a
-            className={clsx(
-              "flex",
-              "items-center",
-              "gap-x-3.5",
-              "py-2 px-2.5",
-              "bg-gray-100",
-              "text-sm",
-              "rounded-lg",
-              "hover:bg-accent1"
-            )}
-          >
-            <Image
-              src="/Calender-icon.svg"
-              width={24}
-              height={24}
-              alt="Calender aside icon"
-              className={clsx("ml-[15px]", "hover:stroke-primary")}
-            />
-            Tus eventos
-          </a>
-        </li>
-        <li className={clsx("mb-2")}>
-          <a
-            className={clsx(
-              "flex",
-              "items-center",
-              "gap-x-3.5",
-              "py-2",
-              "px-2.5",
-              "bg-gray-100",
-              "text-sm",
-              "rounded-lg",
-              "hover:bg-accent1"
-            )}
-          >
-            <Image
-              src="/Tutorials-icon.svg"
-              width={24}
-              height={24}
-              alt="Tutorials aside icon"
-              className={clsx("ml-[15px]", "hover:stroke-primary")}
-            />
-            Tutorials
-          </a>
-        </li>
-      </ul>
-    </nav>
+      </li>
+    </ul>
+
+
+  </nav>
   );
 }
