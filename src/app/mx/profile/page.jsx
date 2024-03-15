8d27c11b2d22a9a115be577bb21ef8b9612de96d
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-03-14 22:10:33 -0600

    saving changes

--- src/app/mx/profile/page.jsx
+++ src/app/mx/profile/page.jsx
@@ -1,71 +1,88 @@
 import clsx from "clsx";
 import Image from "next/image";
 import { TitleSection } from "@/app/components/common";
+import { HeroAdmin } from "@/app/components/common";
+import { BtnPrimary } from "@/app/components/common";
 
 export default function Profile() {
-
+    const dataUser =[
+        {
+            title:"Amigos",
+            num:5,
+        },
+        {
+            title:"Salidas",
+            num:8,
+        },
+        {
+            title:"Eventos",
+            num:1,
+        },
+    ]
   return (
-    <section className="flex flex-wrap">
+    <>
+    <main>
         <TitleSection 
-        urlIcon="/Avatar.svg"
+        urlIcon="/person_icon.svg"
         title="Perfil"
         />
-      <div className={clsx(
-            "bg-gradient-to-br from-[#F5F5F5] to-75% to-secondary",
-            "rounded-2xl",
-            "m-2 p-[25px]",
-            "w-full",
-            "flex",
-            "md:p-10 ",
-            // "lg:w-[1000px]"
-          )}>
-        <div className={clsx(
-            "justify-start"
-        )}
+        <HeroAdmin 
+        title="Nina Vidales"
         >
-          <p className="font-heading text-h3">Nina Vidales</p>
-          <p className="font-heading text-regularSemiBold">
-            ninavidales@email.com
-          </p>
-          <p className="font-heading text-regularSemiBold">331245678</p>
-        </div>
-        <div className={clsx(
-              "absolute flex justify-center",
-              "right-0",
-              "flex-wrap",
-              "w-[220px]",
-            //   "ml-10",
-            )}>
-          <Image
-            src="/ProfilePicture-demo.webp"
-            width={120}
-            height={120}
-            alt="Profile picture"
-            className="drop-shadow-lg mb-6"
-          />
-          <div className="w-[200px] mr-[25px] grid grid-cols-3 text-right">
-            <span className="mx-2">
-              <p className="font-heading text-h3">45</p>
-              <p className="font-heading text-regular">Amigos</p>
-            </span>
-            <span className="mx-2">
-              <p className="font-heading text-h3">35</p>
-              <p className="font-heading text-regular">Salidas</p>
-            </span>
-            <span className="mx-2">
-              <p className="font-heading text-h3">35</p>
-              <p className="font-heading text-regular">Eventos</p>
-            </span>
-          </div>
-        </div>
-      </div>
-      <div>
-        <button className={clsx(
-            "bg-azulGris500",
-            "font-body text-regular text-white"
-        )}>Editar</button>
-        <button>Cambiar imagen</button>
-      </div>
-    </section>
+        <p className="font-heading text-regularSemiBold">ninavidales@email.com</p>
+        <p className="font-heading text-regularSemiBold">331245678</p>
+        </HeroAdmin>
+        <section className="mt-4">
+            <div>
+                <button      
+                    className={clsx(
+                    "w-[180px] h-[45px]",
+                    "rounded-xl",
+                    // "m-8",
+                    "bg-azulGris500",
+                    "font-body text-regular text-white",
+                    "justify-center",
+                    "absolute right-4 -bottom-10",
+                    "md:static"
+                    )}>Editar</button>
+                <button
+                      className={clsx(
+                        "w-[180px] h-[45px]",
+                        "rounded-xl",
+                        // "m-8",
+                        "bg-secondary",
+                        "font-body text-regular text-accent2",
+                        "justify-center",
+                        "absolute right-4 -bottom-10",
+                        "md:static"
+                      )}>Cambiar imagen</button>
+            </div>
+            <div>
+                <img></img>
+                <span className="grid grid-cols-3">
+                    {dataUser.map((item, index) => {
+                        
+                    })}
+                </span>
+            </div>
+        </section>
+        {/* <BtnPrimary 
+        title="Editar"
+        onClick={onclick}
+        /> */}
+
+        <TitleSection 
+        urlIcon="/person_icon.svg"
+        title="Amigos"
+        />
+
+        <TitleSection 
+        urlIcon="/Icono-Fecha.svg"
+        title="Invitaciones pendientes de aceptar"
+        />      
+
+    </main>
+    </>
+
   );
 }
