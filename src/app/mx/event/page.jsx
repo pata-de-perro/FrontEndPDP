ae9b4d42d21ba192973c4a628dc94b74f9f16cb1
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-03-15 16:52:56 -0600

    page to visualize the event created

---
+++ src/app/mx/event/page.jsx
@@ -0,0 +1,49 @@
+import { PlacesList } from "@/app/components/PlacesList";
+import clsx from "clsx";
+
+export default function EventPage() {
+  return (
+    <div>
+      <section className="mt-4">
+        <div className="infoContainer ">
+          <div className="flex">
+            <p className="font-heading text-accent2 text-h4">
+              Nombre de tu evento
+            </p>
+            <img
+              src="/restaurant.svg"
+              width={35}
+              height={35}
+              alt="trip icon"
+              className=""
+            />
+          </div>
+          <div className="flex items-center">
+            <img
+              src="/calendar_today.svg"
+              width={21}
+              height={21}
+              alt="icon calendar today"
+            />
+            <p className="font-body text-inviteSmallText text-azulGris600 ml-2">
+              15 Mayo - 17 Mayo
+            </p>
+          </div>
+        </div>
+      </section>
+        <section className="mt-2">
+        <div className="bg-primary rounded-t-2xl h-[150px] mr-2 "/>
+            <div className="grid grid-cols-3 grid-gap-1">
+                <div className="grid-span-1">
+                    <h3 className="fond-heading text-h3 ml-4">Lugares de interés</h3>
+                    <PlacesList />
+                </div>
+                <div className="map-container grid-span-2">
+
+                </div>
+            </div>
+
+        </section>
+    </div>
+  );
+}
