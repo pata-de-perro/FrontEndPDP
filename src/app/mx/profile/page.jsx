442e5e2633860edec9c4c15083283f059056f588
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-03-14 23:37:24 -0600

    not Invite component created

--- src/app/mx/profile/page.jsx
+++ src/app/mx/profile/page.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { TitleSection } from "@/app/components/common";
 import { HeroAdmin } from "@/app/components/common";
 import { BtnPrimary } from "@/app/components/common";
+import { NotInvite } from "@/app/components/events/NotInvite";
 
 export default function Profile() {
     const dataUser =[
@@ -28,12 +29,16 @@ export default function Profile() {
         />
         <HeroAdmin 
         title="Nina Vidales"
+        urlImg="/profile-pic.png"
         >
         <p className="font-heading text-regularSemiBold">ninavidales@email.com</p>
         <p className="font-heading text-regularSemiBold">331245678</p>
         </HeroAdmin>
         <section className="mt-4">
-            <div>
+            <div className={clsx(
+                "mb-16",
+                "flex justify-around"
+            )}>
                 <button      
                     className={clsx(
                     "w-[180px] h-[45px]",
@@ -57,14 +62,37 @@ export default function Profile() {
                         "md:static"
                       )}>Cambiar imagen</button>
             </div>
-            <div>
-                <img></img>
-                <span className="grid grid-cols-3">
+            {/* <div className="grid grid-cols-1 justify-items-center w-fit bg-accent1">
+                <div className={clsx(
+                    "w-[200px] h-[200px]",
+                    "rounded-full",
+                    // "flex justify-center",
+                    "bg-red-500"
+                )}></div>
+                <span className={clsx(
+                    "grid grid-cols-3 justify-items-end",
+                    // "ml-2",
+                    "w-fit",
+                    // "bg-red-500"
+                )}>
                     {dataUser.map((item, index) => {
-                        
+                        return(
+                            <div key={index}>
+                                <h2 className={clsx(
+                                    "font-heading text-h4",
+                                    "text-accent2 text-center",
+                                    // "border-2"
+                                    )}>{item.num}</h2>
+                                <p className={clsx(
+                                    "font-body text-regular",
+                                    "mx-4",
+                                    // "border",
+                                )}>{item.title}</p>
+                                 </div>
+                        )
                     })}
                 </span>
-            </div>
+            </div> */}
         </section>
         {/* <BtnPrimary 
         title="Editar"
@@ -72,14 +100,21 @@ export default function Profile() {
         /> */}
 
         <TitleSection 
-        urlIcon="/person_icon.svg"
+        urlIcon="/friends-icon.svg"
         title="Amigos"
         />
+        <section className="h-[150px]">
+
+        </section>
 
         <TitleSection 
         urlIcon="/Icono-Fecha.svg"
         title="Invitaciones pendientes de aceptar"
         />      
+
+        <section className="w-full">
+            <NotInvite />
+        </section>
 
     </main>
     </>
