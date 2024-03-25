5d2e1c058533dc3083060fd18ee5771215146444
Josué Yovani <josue.yovani@outlook.com>
2024-03-25 11:10:00 -0600

    Add - get user events to component

--- src/components/events/ReviewPlans/ReviewPlans.jsx
+++ src/components/events/ReviewPlans/ReviewPlans.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { getUserEventsApi } from "@/services";
 import { TitleSection } from "@/components/layouts";
 import { NotEvents } from "@/components/events";
 
 export function ReviewPlans() {
+  const { data: session } = useSession();
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const { data } = await getUserEventsApi(
+          session.user.id,
+          session.user.accessToken
+        );
+        setEvents(data);
+      } catch (error) {}
+    };
+    fetchEvents();
+  }, []);
+
   return (
     <>
       <TitleSection title="Revisa tus planes" urlIcon="/Icono-Fecha.svg" />
-      <NotEvents title="Sin registro de planes" />
+      {events.length === 0 && <NotEvents title="Sin registro de planes" />}
     </>
   );
 }
