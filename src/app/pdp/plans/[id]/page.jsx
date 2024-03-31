f8313500f04dfd299b5a05ebabb72afd64e1b74a
Josué Yovani <josue.yovani@outlook.com>
2024-03-30 23:33:17 -0600

    Add - MakePlan component to PlanEventPage

--- src/app/pdp/plans/[id]/page.jsx
+++ src/app/pdp/plans/[id]/page.jsx
@@ -1,17 +1,17 @@
 import { HeroAuth, TitleSection } from "@/components/layouts";
-import { GoogleMap } from "@/components/maps";
+import { MakePlan } from "@/components/plans";
 
-export default function TravelPage({ params }) {
+export default function PlanEventPage({ params }) {
   return (
     <div>
-      <HeroAuth title="¡Realiza tú plan!">
+      <HeroAuth title="¡Realiza tú plan!" urlImg="/World-bro.svg">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
       </HeroAuth>
       <TitleSection title="Revisa tu mapa" urlIcon="/Icono-Fecha.svg" />
-      <GoogleMap />
+      <MakePlan plan={params.id} />
     </div>
   );
 }
