327e251928aa2796c9dc220216d07183df3d8185
Josué Yovani <josue.yovani@outlook.com>
2024-03-31 01:05:18 -0600

    Add - Get plan event with api

--- src/components/plans/MakePlan/MakePlan.jsx
+++ src/components/plans/MakePlan/MakePlan.jsx
@@ -1,17 +1,35 @@
-import { GoogleMap } from "@/components/maps";
 import clsx from "clsx";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getPlanEventApi } from "@/services";
+import { GoogleMap } from "@/components/maps";
 
-export function MakePlan({ plan }) {
-  console.log(plan);
+export async function MakePlan({ plan }) {
+  const { user } = await getServerSession(authOptions);
+  const { data } = await getPlanEventApi(plan, user.accessToken);
+
   return (
     <div
       className={clsx("h-[600px]", "flex justify-between gap-4", "mt-4 mr-6")}
     >
       <div className={clsx("w-3/4")}>
-        <GoogleMap></GoogleMap>
+        <GoogleMap coords={data?.coordsEvent}></GoogleMap>
       </div>
-      <aside className={clsx("bg-neutral-100", "w-1/4", "rounded-xl", "p-2")}>
-        points
+      <aside
+        className={clsx(
+          "bg-primary/10",
+          "w-1/4",
+          "rounded-xl",
+          "p-4",
+          "relative"
+        )}
+      >
+        <h3 className={clsx("font-semibold text-lg text-primary")}>
+          {data?.title}
+        </h3>
+        <span className={clsx("font-medium text-sm text-accent2")}>
+          {data?.locationEvent}
+        </span>
       </aside>
     </div>
   );
