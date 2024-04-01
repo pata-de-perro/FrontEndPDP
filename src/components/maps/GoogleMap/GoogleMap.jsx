1036c0421c9839a3046fbf939fa8d262cf056edc
Josué Yovani <josue.yovani@outlook.com>
2024-03-31 20:25:52 -0600

    Refactor - Component GoogleMap

--- src/components/maps/GoogleMap/GoogleMap.jsx
+++ src/components/maps/GoogleMap/GoogleMap.jsx
@@ -1,13 +1,11 @@
 "use client";
 import { useEffect, useRef } from "react";
-import clsx from "clsx";
 import { Loader } from "@googlemaps/js-api-loader";
-
+import clsx from "clsx";
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY_GOOGLE;
 
-export function GoogleMap({ coords, children }) {
-  const mapContainerRef = useRef(null);
-  let map = null;
+export function GoogleMap({ optionsMap, chidren }) {
+  const mapGoogleRef = useRef(null);
 
   useEffect(() => {
     const initializeMap = async () => {
@@ -16,27 +14,17 @@ export function GoogleMap({ coords, children }) {
         version: "weekly",
       });
 
-      loader
-        .importLibrary("maps")
-        .then(() => {
-          const google = window.google;
-          map = new google.maps.Map(mapContainerRef.current, {
-            center: { lat: coords[0], lng: coords[1] },
-            zoom: 13,
-          });
-        })
-        .catch((error) => {
-          console.error("Error loading Google Maps API:", error);
-        });
+      const { Map } = await loader.importLibrary("maps");
+      new Map(mapGoogleRef.current, optionsMap);
     };
 
     initializeMap();
-  }, []);
+  }, [optionsMap]);
 
   return (
     <div className={clsx("h-full")}>
-      <div ref={mapContainerRef} className={clsx("h-full", "rounded-xl")}>
-        {children}
+      <div ref={mapGoogleRef} className={clsx("h-full", "rounded-xl")}>
+        {chidren}
       </div>
     </div>
   );
