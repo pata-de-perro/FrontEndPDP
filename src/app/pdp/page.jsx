788a8fa2de43d066709da240614848506d8e46c3
Josué Yovani <josue.yovani@outlook.com>
2024-04-03 11:21:59 -0600

    Refactor - get ession and get events

--- src/app/pdp/page.jsx
+++ src/app/pdp/page.jsx
@@ -1,28 +1,21 @@
-"use client";
-import { useRouter } from "next/navigation";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { HeroAuth } from "@/components/layouts";
-import { BtnPrimary } from "@/components/common";
 import { ReviewPlans } from "@/components/events";
 
-export default function HomePage() {
-  const router = useRouter();
+export default async function HomePage() {
+  const { user } = await getServerSession(authOptions);
 
   return (
     <>
-      <main>
-        <HeroAuth title="¡Facilitando tus planes!" urlImg="/Reminders-bro.svg">
-          <p>
-            Queremos que el planear tu viaje o salida sea lo más fácil posible.
-            En Pata de Perro puedes guardar diferentes ubicaciones segun su
-            categoría.
-          </p>
-          <BtnPrimary
-            title="Planea un evento"
-            onClick={() => router.push("/pdp/events/create")}
-          />
-        </HeroAuth>
-        <ReviewPlans />
-      </main>
+      <HeroAuth title="¡Facilitando tus planes!" urlImg="/Reminders-bro.svg">
+        <p>
+          Queremos que el planear tu viaje o salida sea lo más fácil posible. En
+          Pata de Perro puedes guardar diferentes ubicaciones segun su
+          categoría.
+        </p>
+      </HeroAuth>
+      <ReviewPlans user={user} />
     </>
   );
 }
--- src/components/events/ReviewPlans/ReviewPlans.jsx
+++ src/components/events/ReviewPlans/ReviewPlans.jsx
@@ -1,31 +1,20 @@
-"use client";
-import { useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
 import { getUserEventsApi } from "@/services";
 import { TitleSection } from "@/components/layouts";
 import { NotEvents, PlansContainer } from "@/components/events";
 
-export function ReviewPlans() {
-  const { data: session } = useSession();
-  const [events, setEvents] = useState([]);
-
-  useEffect(() => {
-    const fetchEvents = async () => {
-      try {
-        const { data } = await getUserEventsApi(
-          session.user.id,
-          session.user.accessToken
-        );
-        setEvents(data);
-      } catch (error) {}
-    };
-    fetchEvents();
-  }, []);
+export async function ReviewPlans({ user }) {
+  const { data: events } = await getUserEventsApi(user.id, user.accessToken);
 
   return (
     <>
       <TitleSection title="Revisa tus planes" urlIcon="/Icono-Fecha.svg" />
-      {events.length === 0 && <NotEvents title="Sin registro de planes" />}
+      {(events === undefined || events?.length === 0) && (
+        <NotEvents
+          title={
+            events === undefined ? "Sin información" : "Sin registro de planes"
+          }
+        />
+      )}
       <PlansContainer data={events} />
     </>
   );
