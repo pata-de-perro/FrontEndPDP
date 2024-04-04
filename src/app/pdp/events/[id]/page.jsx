f512f606f8fe87000eb905d5818ca3e16a3ef2af
Josué Yovani <josue.yovani@outlook.com>
2024-04-03 18:41:32 -0600

    Add - Connect to api event page

--- src/app/pdp/events/[id]/page.jsx
+++ src/app/pdp/events/[id]/page.jsx
@@ -1,11 +1,13 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getPlacesEventApi } from "@/services";
 import { HeroAuth } from "@/components/layouts";
 import { ContainerEvent } from "@/components/events";
 
 export default async function EventPage({ params }) {
   const eventId = params.id;
   const { user } = await getServerSession(authOptions);
+  const { data: event } = await getPlacesEventApi(eventId, user.accessToken);
 
   return (
     <>
@@ -14,7 +16,7 @@ export default async function EventPage({ params }) {
         urlImg="/World-amico.svg"
         type="primary"
       ></HeroAuth>
-      <ContainerEvent />
+      <ContainerEvent event={event} />
     </>
   );
 }
