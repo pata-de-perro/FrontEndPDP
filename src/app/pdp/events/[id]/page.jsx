0dff36e2cbf2b047f6d371784fce1aa31802d0aa
Josué Yovani <josue.yovani@outlook.com>
2024-04-03 18:40:30 -0600

    Add - Session user to event page

--- src/app/pdp/events/[id]/page.jsx
+++ src/app/pdp/events/[id]/page.jsx
@@ -1,7 +1,12 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { HeroAuth } from "@/components/layouts";
 import { ContainerEvent } from "@/components/events";
 
 export default async function EventPage({ params }) {
+  const eventId = params.id;
+  const { user } = await getServerSession(authOptions);
+
   return (
     <>
       <HeroAuth
