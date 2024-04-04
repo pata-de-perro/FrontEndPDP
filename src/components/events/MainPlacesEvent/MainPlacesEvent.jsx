6e94f0cb2482cab5e9e74bfaa8126d2ec9faea81
Josué Yovani <josue.yovani@outlook.com>
2024-04-03 20:23:16 -0600

    Add - Render list places

--- src/components/events/MainPlacesEvent/MainPlacesEvent.jsx
+++ src/components/events/MainPlacesEvent/MainPlacesEvent.jsx
@@ -5,14 +5,19 @@ export function MainPlacesEvent({ event, mapId }) {
   const { coordsEvent, locations } = event;
   const ubicationMap = { lat: coordsEvent[0], lng: coordsEvent[1] };
 
-  console.log(event);
-
   return (
     <main className={clsx("h-[600px]", "flex flex-col gap-4", "mt-4")}>
       <section className={clsx("h-full", "flex justify-between gap-4")}>
-        <aside
-          className={clsx("bg-primary/10", "w-1/4", "rounded-xl", "p-2")}
-        ></aside>
+        <aside className={clsx("bg-primary/10", "w-1/4", "rounded-xl", "p-2")}>
+          <ul>
+            {locations?.map((place) => (
+              <li key={place._id}>
+                <span className="text-xs font-medium">{place.name}</span>
+                <p className="text-xs">{place.simpleAddress}</p>
+              </li>
+            ))}
+          </ul>
+        </aside>
         <section className={clsx("w-3/4")}>
           <GoogleMap
             ubicationMap={ubicationMap}
