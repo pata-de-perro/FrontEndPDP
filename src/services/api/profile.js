dafacf8e8f48497cd96c98dc4715616f45930546
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-04-04 17:54:08 -0600

    services api profile file added

---
+++ src/services/api/profile.js
@@ -0,0 +1,32 @@
+const URL_API = process.env.NEXT_PUBLIC_URL_API;
+
+export const getProfileApi = async (userId, token) => {
+    try{
+        const res = await fetch(`${URL_API}/profile/${userId}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        throw new Error(`Error fetching user profile: ${err.message}`)
+    }
+}
+
+export const updateProfileApi = async (userId, token, newData) => {
+    try{
+        const res = await fetch(`${URL_API}/profile-update/${userId}`, {
+            method: 'PUT',
+            headers:{
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return
+    }catch (err) {
+        throw new Error(`Error fetching user profile: ${err.message}`)
+    }
+}
--- src/services/index.js
+++ src/services/index.js
@@ -2,3 +2,4 @@ export * from "./api/auth";
 export * from "./api/events";
 export * from "./api/locations";
 export * from "./api/users";
+export * from "./api/profile";
