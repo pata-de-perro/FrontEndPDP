a6d7d2d2e1d5ff5a25be1552860ea8421dd1d546
Josué Yovani <josue.yovani@outlook.com>
2024-04-04 10:33:20 -0600

    Add - Title Aside sction and change order layout

--- src/components/events/MainPlacesEvent/MainPlacesEvent.jsx
+++ src/components/events/MainPlacesEvent/MainPlacesEvent.jsx
@@ -9,17 +9,20 @@ export function MainPlacesEvent({ event, mapId }) {
   return (
     <main className={clsx("h-[700px]", "flex flex-col gap-4", "mt-4")}>
       <section className={clsx("h-full", "flex justify-between gap-4")}>
-        <section className={clsx("w-3/4")}>
-          <GoogleMap
-            ubicationMap={ubicationMap}
-            mapId={mapId}
-            locations={locations}
-          ></GoogleMap>
-        </section>
         <aside
           className={clsx("bg-secondary/10", "w-1/4", "rounded-xl", "p-2")}
         >
           <div className={clsx("flex flex-col")}>
+            <h3
+              className={clsx(
+                "text-lg font-semibold text-accent2",
+                "text-center",
+                "my-4"
+              )}
+            >
+              Tus lugares elejidos
+            </h3>
+
             {placesOfInterestMenu.map((placeMenuCategory) => (
               <div key={placeMenuCategory.key} className={clsx("mb-3")}>
                 <div className={clsx("flex items-center", "border-t-0")}>
@@ -54,6 +57,13 @@ export function MainPlacesEvent({ event, mapId }) {
             ))}
           </div>
         </aside>
+        <section className={clsx("w-3/4")}>
+          <GoogleMap
+            ubicationMap={ubicationMap}
+            mapId={mapId}
+            locations={locations}
+          ></GoogleMap>
+        </section>
       </section>
       <section className={clsx("h-5", "bg-secondary", "rounded-xl")}></section>
     </main>
