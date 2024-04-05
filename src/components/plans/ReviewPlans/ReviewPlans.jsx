ee7fe5ed523a5c8d26c84810f9d1a86e21f09406
Josué Yovani <josue.yovani@outlook.com>
2024-04-05 12:17:22 -0600

    Feat - Implement get upcoming events by user api

--- src/components/plans/ReviewPlans/ReviewPlans.jsx
+++ src/components/plans/ReviewPlans/ReviewPlans.jsx
@@ -1,20 +1,26 @@
 import clsx from "clsx";
-import { getUserEventsApi } from "@/services";
+import { getUpcomingEventsByUserApi } from "@/services";
 import { TitleSection } from "@/components/layouts";
 import { NotEvents } from "@/components/common";
 import { ListPlans } from "@/components/plans";
 
 export async function ReviewPlans({ user }) {
-  const { data: events } = await getUserEventsApi(user.id, user.accessToken);
+  const { id: userId, accessToken } = user;
+
+  const { data: events } = await getUpcomingEventsByUserApi(
+    userId,
+    accessToken
+  );
 
   return (
     <div className={clsx("mt-10 mr-4")}>
-      <TitleSection title="Revisa tus planes" urlIcon="/Icono-Fecha.svg" />
+      <TitleSection
+        title="Revisa tus próximos planes"
+        urlIcon="/Icono-Fecha.svg"
+      />
       {(events === undefined || events?.length === 0) && (
         <NotEvents
-          title={
-            events === undefined ? "Sin información" : "Sin registro de planes"
-          }
+          title={events === undefined ? "Sin planes" : "Sin planes próximos"}
         />
       )}
       <ListPlans data={events} />
