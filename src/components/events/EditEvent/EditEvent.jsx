cf86be58789e07fd4dc7b5c82d542d5e79a10f62
Josué Yovani <josue.yovani@outlook.com>
2024-04-05 03:21:52 -0600

    Add - UseForm for event editing form handling

--- src/components/events/EditEvent/EditEvent.jsx
+++ src/components/events/EditEvent/EditEvent.jsx
@@ -1,6 +1,29 @@
+import { useForm } from "react-hook-form";
+
 export function EditEvent({ user, event }) {
   const { accessToken } = user;
   const { _id, isTravel, title, description, initialDate, endDate } = event;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    setValue,
+    reset,
+  } = useForm({
+    defaultValues: {
+      isTravel,
+      title,
+      description,
+      initialDate,
+      endDate,
+    },
+  });
+
+  const onSubmitEditEvent = handleSubmit((data) => {
+    console.log(data);
+  });
 
   return (
     <div>
