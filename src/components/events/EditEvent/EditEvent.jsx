6c0b9dd5ef372012b5c0c46f5594fb3e7ca2c4f1
Josué Yovani <josue.yovani@outlook.com>
2024-04-05 05:03:22 -0600

    Add - formatDatesEvent helper in event editing component

--- src/components/events/EditEvent/EditEvent.jsx
+++ src/components/events/EditEvent/EditEvent.jsx
@@ -1,10 +1,17 @@
-import { useForm } from "react-hook-form";
-import { FormEditEvent } from "@/components/events";
 import clsx from "clsx";
+import { useForm } from "react-hook-form";
+import { formatDatesEvent } from "@/helpers";
+import { FormEditEvent } from "@/components/events";
 
 export function EditEvent({ user, event }) {
   const { accessToken } = user;
   const { _id, isTravel, title, description, initialDate, endDate } = event;
+
+  const { formattedStartDate, formattedEndDate } = formatDatesEvent(
+    isTravel,
+    initialDate,
+    endDate
+  );
 
   const {
     register,
@@ -16,8 +23,8 @@ export function EditEvent({ user, event }) {
       isTravel,
       title,
       description,
-      initialDate,
-      endDate,
+      initialDate: formattedStartDate,
+      endDate: formattedEndDate,
     },
   });
 
