dbff0280b624c2d0e4cc0ff4201923bfc4d23b88
Josué Yovani <josue.yovani@outlook.com>
2024-04-05 03:16:17 -0600

    Add - Information destructuring of props

--- src/components/events/EditEvent/EditEvent.jsx
+++ src/components/events/EditEvent/EditEvent.jsx
@@ -1,3 +1,12 @@
-export function EditEvent() {
-  return <div>Editando...</div>;
+export function EditEvent({ user, event }) {
+  const { accessToken } = user;
+  const { _id, isTravel, title, description, initialDate, endDate } = event;
+
+  return (
+    <div>
+      <h2>Editando...{_id}</h2>
+      <span>{title}</span>
+      <span>{description}</span>
+    </div>
+  );
 }
