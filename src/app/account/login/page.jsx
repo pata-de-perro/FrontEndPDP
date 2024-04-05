10b395fad6fed46718bb26d364f4923a02a720b5
Josué Yovani <josue.yovani@outlook.com>
2024-04-05 12:27:26 -0600

    Add - Verify exist session user to login page

--- src/app/account/login/page.jsx
+++ src/app/account/login/page.jsx
@@ -1,13 +1,18 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 import Link from "next/link";
-import Image from "next/image";
-import clsx from "clsx";
 import { PerformLogin } from "@/components/account";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const user = await getServerSession(authOptions);
+  if (user) {
+    redirect("/pdp");
+  }
+
   return (
     <>
-      <div
-        className="grid md:grid-cols-2 lg:grid-cols-1 justify-items-center lg:justify-items-start">
+      <div className="grid md:grid-cols-2 lg:grid-cols-1 justify-items-center lg:justify-items-start">
         <h1 className="text-h1 font-heading text-center text-azulGris900 pt-[15px]">
           Inicia sesión en
         </h1>
@@ -20,16 +25,16 @@ export default function LoginPage() {
             className="lg:w-[420px] lg:h-[auto]"
           />
         </Link>
-        </div>
-        <div className="flex flex-col lg:flex-row">
-          <p className="text-regular font-body text-center text-azulGris900">
-            ¿Eres un nuevo usuario?
-          </p>
-          <p className="text-regularBold font-body text-center text-azulGris900 lg:ml-4">
-            <Link href="/account/register">¡Registrate ahora!</Link>
-          </p>
-        </div>
-        <PerformLogin />
+      </div>
+      <div className="flex flex-col lg:flex-row">
+        <p className="text-regular font-body text-center text-azulGris900">
+          ¿Eres un nuevo usuario?
+        </p>
+        <p className="text-regularBold font-body text-center text-azulGris900 lg:ml-4">
+          <Link href="/account/register">¡Registrate ahora!</Link>
+        </p>
+      </div>
+      <PerformLogin />
     </>
   );
 }
