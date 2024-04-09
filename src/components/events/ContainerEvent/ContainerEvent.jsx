b6856068bb61cad7645a3134bbb3611661030c6c
Josué Yovani <josue.yovani@outlook.com>
2024-04-09 16:27:13 -0600

    Styles - Change display ActionsEvent

--- src/components/events/ContainerEvent/ContainerEvent.jsx
+++ src/components/events/ContainerEvent/ContainerEvent.jsx
@@ -6,12 +6,15 @@ const MAP_ID = process.env.ID_MAP_GOOGLE;
 
 export function ContainerEvent({ event, user }) {
   return (
-    <div className={clsx("mt-4 mr-6 text-azulGris900")}>
-      <TitleSection
-        title="Revisa tu lugares elejidos"
-        urlIcon={<FiMap />}
-      />
-      <ActionsEvent user={user} event={event} />
+    <div className={clsx("mr-6")}>
+      <div className={clsx("flex items-center justify-between")}>
+        <TitleSection
+          title="Revisa tu mapa"
+          urlIcon={<FiMap />}
+          type="primary"
+        />
+        <ActionsEvent user={user} event={event} />
+      </div>
       <MainPlacesEvent event={event} mapId={MAP_ID} />
     </div>
   );
