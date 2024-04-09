b08a63011162e860297f176b5a30847efd914ba1
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-04-09 01:27:35 -0600

    profile view page adding style

--- src/app/pdp/profile/page.jsx
+++ src/app/pdp/profile/page.jsx
@@ -1,4 +1,3 @@
-import clsx from "clsx";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { HeroAuth } from "@/components/layouts";
@@ -12,16 +11,6 @@ export default async function Profile() {
   const token = session.user.accessToken;
   const profileData = (await getProfileApi(session.user.id, token)).data;
 
-  const initials = () => {
-    const fullName = profileData.name;
-    const separeteName = fullName.split(" ");
-    const firstIn = separeteName[0].charAt(0);
-    const secondIn = separeteName[1].charAt(0);
-    const nameInitials = firstIn + secondIn;
-
-    return nameInitials;
-  };
-
   return (
     <>
       <HeroAuth title="Perfil de usuario">
@@ -30,8 +19,9 @@ export default async function Profile() {
           <AvatarInitials data={profileData}/>
         </div>
       </HeroAuth>
-      
+
       <ProfileDataView data={profileData}/>
+      <img src="/World.svg" className="w-[250px] h-auto absolute right-0 bottom-0 -z-10"></img>
     </>
   );
 }
--- src/components/profileView/ProfileData/ProfileData.jsx
+++ src/components/profileView/ProfileData/ProfileData.jsx
@@ -2,27 +2,34 @@ import clsx from "clsx";
 
 import { birthdayDateProfile } from "@/helpers";
 
-export function ProfileDataView({ data }){
-    
-    return(
-        <>
-        <div className="w-[350px] h-[250px] border mt-4 rounded-2xl p-4">
-        <p className="font-heading text-regularSemiBold">
-          {data.name} 
-        </p>
-        <p className="font-heading text-regularSemiBold">
-          {data.email} 
-        </p>
-        <p className="font-heading text-regularSemiBold">
-          {data.phoneNumber}
-        </p>
-        <p className="font-heading text-regularSemiBold">
-          {birthdayDateProfile(data.birthdate)}
-        </p>
-        <p className="font-heading text-regularSemiBold">
-          {data.gender}
-        </p>
+export function ProfileDataView({ data }) {
+  return (
+    <>
+      <div className="p-4">
+        <h2 className="font-heading text-h2">Datos de perfil</h2>
+        <div>
+          <span className="font-heading text-regular">Nombre </span>
+          <span className="font-heading text-regularSemiBold">{data.name}</span>
         </div>
-        </>
-    )
+        <div>
+          <span className="font-heading text-regular">Correo electrónico </span>
+          <span className="font-heading text-regularSemiBold">
+            {data.email}
+          </span>
+        </div>
+        <div>
+          <span className="font-heading text-regular">Teléfono </span>
+          <span className="font-heading text-regularSemiBold">
+            {data.phoneNumber}
+          </span>
+        </div>
+        <div>
+        <span className="font-heading text-regular">Cumpleaños </span>
+        <span className="font-heading text-regularSemiBold">
+          {birthdayDateProfile(data.birthdate)}
+        </span>
+        </div>
+      </div>
+    </>
+  );
 }
