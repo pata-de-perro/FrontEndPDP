b69c3132a71879604910ce89337e3bb8d31166b7
Josué Yovani <josue.yovani@outlook.com>
2024-04-10 17:20:56 -0600

    Change - Rendering of the places chosen by categories

--- src/components/events/MainPlacesEvent/MainPlacesEvent.jsx
+++ src/components/events/MainPlacesEvent/MainPlacesEvent.jsx
@@ -10,9 +10,7 @@ export function MainPlacesEvent({ event, mapId }) {
   return (
     <main className={clsx("h-[800px]", "flex flex-col gap-4", "mt-4")}>
       <section className={clsx("h-full", "flex justify-between gap-4")}>
-        <aside
-          className={clsx("bg-azulGris50", "w-1/4", "rounded-xl", "p-2")}
-        >
+        <aside className={clsx("bg-azulGris50", "w-1/4", "rounded-xl", "p-2")}>
           <div className={clsx("flex flex-col")}>
             <h3
               className={clsx(
@@ -24,38 +22,42 @@ export function MainPlacesEvent({ event, mapId }) {
               Tus lugares elejidos
             </h3>
 
-            {placesOfInterestMenu.map((placeMenuCategory) => (
-              <div key={placeMenuCategory.key} className={clsx("mb-3")}>
-                <div className={clsx("flex items-center", "border-t-0")}>
-                  <img
-                    src={placeMenuCategory.pinUrl}
-                    alt="gps pin icon"
-                    className="h-12 w-auto"
-                  />
-                  <span
-                    className={clsx(
-                      "font-body text-regularSemiBold text-azulGris900",
-                      "ml-2 mb-2"
-                    )}
-                  >
-                    {placeMenuCategory.title}
-                  </span>
+            {placesOfInterestMenu
+              .filter((placeMenuCategory) =>
+                locations.some((location) =>
+                  placeMenuCategory.typePlace.includes(location.type)
+                )
+              )
+              .map((placeMenuCategory) => (
+                <div key={placeMenuCategory.key} className={clsx("mb-3")}>
+                  <div className={clsx("flex items-center", "border-t-0")}>
+                    <img
+                      src={placeMenuCategory.pinUrl}
+                      alt="gps pin icon"
+                      className="h-12 w-auto"
+                    />
+                    <span
+                      className={clsx(
+                        "font-body text-regularSemiBold text-azulGris900",
+                        "ml-2 mb-2"
+                      )}
+                    >
+                      {placeMenuCategory.title}
+                    </span>
+                  </div>
+                  <ul className="pl-4 list-disc ml-6">
+                    {locations.map((location) => {
+                      if (placeMenuCategory.typePlace.includes(location.type)) {
+                        return (
+                          <li key={location._id}>
+                            <a className="text-xs font-body">{location.name}</a>
+                          </li>
+                        );
+                      }
+                    })}
+                  </ul>
                 </div>
-                <ul className="pl-4 list-disc ml-6">
-                  {locations.map((location) => {
-                    if (placeMenuCategory.typePlace.includes(location.type)) {
-                      return (
-                        <li key={location._id}>
-                          <a className="text-xs font-body">
-                            {location.name}
-                          </a>
-                        </li>
-                      );
-                    }
-                  })}
-                </ul>
-              </div>
-            ))}
+              ))}
           </div>
         </aside>
         <section className={clsx("w-3/4")}>
